54adc87c2b759fe3555489fe6dfb4917f6bfcd4b
barannkoca <barannkoca@gmail.com>
2025-04-05 21:07:37 +0300

    5

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,5 +4,4 @@ import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
 
-export const GET = handler.handlers.GET;
-export const POST = handler.handlers.POST;
+export { handler as GET, handler as POST }
