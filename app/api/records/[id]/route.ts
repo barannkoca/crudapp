634d696526f63934989a24198c9d1b0c86417293
barannkoca <barannkoca@gmail.com>
2025-04-30 21:58:04 +0300

    fix

--- app/api/records/[id]/route.ts
+++ app/api/records/[id]/route.ts
@@ -4,10 +4,8 @@ import { authOptions } from "@/lib/auth";
 import { Record } from "@/models/Record";
 import connectDB from "@/lib/mongodb";
 
-export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
+export async function GET(request: Request, props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   try {
     const session = await getServerSession(authOptions);
     if (!session?.user?.id) {
